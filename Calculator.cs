77e6be99628a45415ab16c710e8aba91c02304c9
adepra22 <ade.prasetyo@students.amikom.ac.id>
2020-03-10 20:44:56 +0700

    Menambahkan Fungsi Perkalian dan Pembagian

--- Calculator.cs
+++ Calculator.cs
@@ -1,25 +1,43 @@
-class Calculator
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
 {
-	static void Main(string[] args)
+	class Calculator
 	{
-		int a = 10;
-		int b = 6;
+		static void Main(string[] args)
+		{
+			int a = 10;
+			int b = 6;
 		
-		Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a,b));
-		Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a,b));
+			Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a,b));
+			Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a,b));
 		
-		Console.WriteLine("\nTekan sembarang key untuk keluar");
-		Console.ReadKey();
-	}
+			Console.WriteLine("\nTekan sembarang key untuk keluar");
+			Console.ReadKey();
+		}
 	
-	static int Penambahan(int a, int b)
-	{
-		return a+b;
-	}
+		static int Penambahan(int a, int b)
+		{
+			return a+b;
+		}
 	
-	static int Pengurangan(int a, int b)
-	{
-		return a-b;
-	}
+		static int Pengurangan(int a, int b)
+		{
+			return a-b;
+		}
+		
+		static int Perkalian(int a, int b)
+        {
+            return a * b;
+        }
+        static float Pembagian(float a, float b)
+        {
+            return a / b;
+        }
 	
+	}
 }
