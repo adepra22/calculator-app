6c176f3b14f185008474e4c4eeb94243871e2eec
adepra22 <ade.prasetyo@students.amikom.ac.id>
2020-03-10 20:49:40 +0700

    Membuat menu Pilihan

--- Calculator.cs
+++ Calculator.cs
@@ -10,12 +10,26 @@ namespace Calculator
 	{
 		static void Main(string[] args)
 		{
+			/*
 			int a = 10;
 			int b = 6;
 		
 			Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a,b));
 			Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a,b));
-		
+			*/
+			
+			Console.Title = "Aplikasi Calculator";
+            Console.WriteLine("Pilih menu Calculator:\n");
+            Console.WriteLine("1. Penambahan");
+            Console.WriteLine("2. Pengurangan");
+            Console.WriteLine("3. Perkalian");
+            Console.WriteLine("4. Pembagian\n");
+            Console.Write("Input nomor menu [1..4]: ");
+            
+            int pil = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+			
 			Console.WriteLine("\nTekan sembarang key untuk keluar");
 			Console.ReadKey();
 		}
